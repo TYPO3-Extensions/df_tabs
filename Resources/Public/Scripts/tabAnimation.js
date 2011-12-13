86266f7064dd3a5d403c071f83b1db846aa04d9f
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-13 19:57:22 +0000

    [FEATURE] Integrate slider script
    Resolves: #31895
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/df_tabs/trunk@55461 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ Resources/Public/Scripts/tabAnimation.js
@@ -0,0 +1,46 @@
+/**
+ * Smooth Image Transition effect
+ *
+ * @author Stefan Galinski <sgalinski@df.eu>
+ */
+var dfTabsSmoothImageTransition = {
+	/**
+	 * Preparation for the animation effects
+	 *
+	 * @param {TabBar} tabBar
+	 * @return {void}
+	 */
+	beforeInitialize: function(tabBar) {
+		tabBar.elementMap.each(function(element) {
+			element.contentItem.set('tween', {
+				duration: 400
+			});
+
+			if (!element.contentItem.hasClass(tabBar.options.classPrefix + 'tabContentSelected')) {
+				element.contentItem.fade('hide');
+			}
+		});
+	},
+
+	/**
+	 * Animates the transition between two tabs
+	 *
+	 * @param {int} nextTabIndex
+	 * @return {void}
+	 */
+	tabChange: function(nextTabIndex) {
+		if (this.previousTab < 0) {
+			return;
+		}
+
+		this.elementMap[this.previousTab].contentItem.fade('out');
+		this.elementMap[nextTabIndex].contentItem.fade('in');
+
+		var selectedClass = this.options.classPrefix + 'tabMenuEntrySelected';
+		this.elementMap.each(function(element) {
+			element.menuItem.removeClass(selectedClass);
+		});
+		this.elementMap[nextTabIndex].menuItem.addClass(selectedClass);
+		this.previousTab = nextTabIndex;
+	}
+};
