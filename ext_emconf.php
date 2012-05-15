c7386376abfdc26ee28e7664dee9fe06f6649a7e
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-15 13:50:43 +0000

    [TASK] Increase maximum TYPO3 dependency to TYPO3 4.7
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/df_tabs/trunk@62250 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -35,7 +35,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'constraints' => array(
 		'depends' => array(
 			'php' => '5.3.0-5.3.99',
-			'typo3' => '4.6.0-4.6.99',
+			'typo3' => '4.6.0-4.7.99',
 		),
 		'conflicts' => array(
 		),
