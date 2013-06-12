796d696a9cffef16ea55430b92f84d7e492e63c4
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-12 14:13:32 +0000

    [FEATURE] Compatibility with 6.0/6.1
    Resolves: #46015
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/df_tabs/trunk@76504 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,8 +4,8 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-t3lib_extMgm::addPItoST43($GLOBALS['_EXTKEY'], 'Classes/Controller/PluginController.php', '_plugin1', 'list_type', 1);
+t3lib_extMgm::addPItoST43('df_tabs', 'Classes/Controller/PluginController.php', '_plugin1', 'list_type', 1);
 
-$TYPO3_CONF_VARS['FE']['eID_include']['dftabs'] = 'EXT:df_tabs/Classes/Ajax/LoadAjax.php';
+$GLOBALS['TYPO3_CONF_VARS']['FE']['eID_include']['dftabs'] = 'EXT:df_tabs/Classes/Ajax/LoadAjax.php';
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -4,35 +4,32 @@ if (!defined('TYPO3_MODE')) {
 	die('Access denied.');
 }
 
-/** @var $TCA array */
-/** @var $_EXTKEY string */
-
-	// remove default plugin fields
+// remove default plugin fields
 t3lib_div::loadTCA('tt_content');
 $list = 'layout,select_key,pages,recursive';
-$TCA['tt_content']['types']['list']['subtypes_excludelist'][$_EXTKEY . '_plugin1'] = $list;
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['df_tabs_plugin1'] = $list;
 
-	// add flexform configuration
-$TCA['tt_content']['types']['list']['subtypes_addlist'][$_EXTKEY . '_plugin1'] = 'pi_flexform';
-t3lib_extMgm::addPiFlexFormValue($_EXTKEY . '_plugin1', 'FILE:EXT:df_tabs/Configuration/FlexForms/flexform.xml');
+// add flexform configuration
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['df_tabs_plugin1'] = 'pi_flexform';
+t3lib_extMgm::addPiFlexFormValue('df_tabs_plugin1', 'FILE:EXT:df_tabs/Configuration/FlexForms/flexform.xml');
 
-	// add plugin
+// add plugin
 t3lib_extMgm::addPlugin(
 	array(
 		'LLL:EXT:df_tabs/Resources/Private/Language/locallang.xlf:tt_content.list_type_plugin1',
-		$_EXTKEY . '_plugin1',
-		t3lib_extMgm::extRelPath($_EXTKEY) . 'Resources/Public/Images/contentElementWizard.png'
+		'df_tabs_plugin1',
+		t3lib_extMgm::extRelPath('df_tabs') . 'Resources/Public/Images/contentElementWizard.png'
 	),
 	'list_type'
 );
 
-	// add static typoscript template
-t3lib_extMgm::addStaticFile($_EXTKEY, 'Configuration/TypoScript/', 'df_tabs');
+// add static typoscript template
+t3lib_extMgm::addStaticFile('df_tabs', 'Configuration/TypoScript/', 'df_tabs');
 
-	// add plugin to the content element wizard list
+// add plugin to the content element wizard list
 if (TYPO3_MODE === 'BE') {
-	$TBE_MODULES_EXT['xMOD_db_new_content_el']['addElClasses']['Tx_DfTabs_Wizards_ContentElementWizard'] =
-		t3lib_extMgm::extPath($_EXTKEY) . 'Classes/Wizards/ContentElementWizard.php';
+	$GLOBALS['TBE_MODULES_EXT']['xMOD_db_new_content_el']['addElClasses']['Tx_DfTabs_Wizards_ContentElementWizard'] =
+		t3lib_extMgm::extPath('df_tabs') . 'Classes/Wizards/ContentElementWizard.php';
 }
 
 ?>
