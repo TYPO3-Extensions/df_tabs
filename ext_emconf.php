7422789fd9f27578641c4f9536a67e037289fef8
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2014-05-30 14:50:27 +0000

    [TASK] Ensure TYPO3 6.2 compatibility
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/df_tabs/trunk@85445 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,8 +34,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'php' => '5.3.0-5.4.99',
-			'typo3' => '4.7.0-6.1.99',
+			'php' => '5.3.0-5.5.99',
+			'typo3' => '4.7.0-6.2.99',
 		),
 		'conflicts' => array(
 		),
